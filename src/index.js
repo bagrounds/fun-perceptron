1a40ce8bb0f80ca6a2a7cc7c5ced2fa49ce2c12b
bagrounds <bagrounds@gmail.com>
2017-05-04 19:54:09 -0700

    [MINOR][CHANGE] classify to return 0 or 1, update to accept w, x, r

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@
   /* exports */
   module.exports = {
     classify: curry(classify),
-    update: curry.options(update, ['w', 'x', 'dy', 'rate'])
+    update: curry(update)
   }
 
   /**
@@ -23,26 +23,24 @@
    * @param {Array} w - weights
    * @param {Array} x - feature vector for an example to classify
    *
-   * @return {Boolean} if the datum is in class 1
+   * @return {Number} 1 or 0 indicating the classification of x
    */
   function classify (w, x) {
-    return vector.dot(w, array.prepend(1, x)) > 0
+    return vector.dot(w, array.prepend(1, x)) > 0 ? 1 : 0
   }
 
   /**
    *
    * @function module:fun-perceptron.update
    *
-   * @param {Object} o - options
-   * @param {Array} o.w - weights
-   * @param {Array} o.x - feature vector for an example to classify
-   * @param {Number} o.rate - learning rate
-   * @param {Array} o.dy - error
+   * @param {Array} w - weights
+   * @param {Array} x - feature vector for an example to classify
+   * @param {Number} r - required output class (label) for this example (0 or 1)
    *
-   * @return {Array} w * (rate * dy) * x
+   * @return {Array} w if x was classified as r, w adjusted by x otherwise
    */
-  function update (o) {
-    return vector.sum(o.w, vector.scale(o.rate * o.dy, array.prepend(1, o.x)))
+  function update (w, x, r) {
+    return vector.sum(w, vector.scale(r - classify(w, x), array.prepend(1, x)))
   }
 })()
 
